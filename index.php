cca0a3c081d3eb533b577b15282336efe0a56e4e
Mikal <skafei11@gmail.com>
2016-11-11 14:47:53 +0100

    Finally getting this bullshit to work, I hope

---
+++ index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Mikal FIELD CV </title>
+    <link rel="stylesheet"  href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/mystyle.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Proza+Libre|Taviraj" rel="stylesheet">
+
+</head>
+
+<body id="canvas" class="row">
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><img id="expo-img"class="img-responsive" src="image/paris.png"/></a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Accueil</a></li>
+                    <li><a href="#">Inscription</a></li>
+                    <li><a href="#">Login admin</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+
+</nav>
+
+
+<section id="section-1" class="col-xs-12">
+
+    <div class="row">
+        <h2 class="col-xs-11 col-xs-offset-1"><i class="fa fa-graduation-cap"></i> Cursus</h2>
+        <p class="col-xs-11 col-xs-offset-1 col-md-7">
+            <span class="date-place">2016-2017 Paris </span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Bachelor Génie Logiciel Web et Mobilité à l’ECEtech <br/>
+            <span class="date-place"> 2014-2016 Paris </span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            BTS SIO (Système informatiques aux organisations) option
+            SLAM (Solutions logicielles et applications métiers) à l’ECEtech<br/>
+            <span class="date-place">2012-2013 Paris</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Baccalauréat général S option Physique au lycée Hélène Boucher
+        </p>
+        <img id="logo-ecetech" class="col-xs-0 col-md-3" src="ecetech_logo.jpg" Alt="ECEtech Logo">
+    </div>
+</section>
+
+
+<section id="section-2" class="col-xs-12">
+
+    <div class="row">
+        <h2 class="col-xs-11 col-xs-offset-1"><i class="fa fa-industry"></i> Expérience</h2>
+        <p class="col-xs-11 col-xs-offset-1 col-md-7">
+            <span class="date-place">2014-2015 ECEtech </span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Application de jeux type "Sim City" codé en C : Gestion des batiments et calcul du plus court chemin<br/>
+            <span class="date-place">2015 Entreprise Systra</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Stage de support technique sur 2 mois : Gestion de stocks et de base de donnée,
+            préparation des ordinateurs<br/>
+            <span class="date-place">2015-2016 Entreprise GPI</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Stage intégration Web sur 2 mois : Création de landing pages responsives
+        </p>
+        <img id="logo-systra" class="col-xs-0 col-md-3" src="Systra.png" Alt="Systra Logo">
+    </div>
+</section>
+
+
+
+<section id="section-3" class="col-xs-12">
+
+    <div class="row">
+        <h2 class="col-xs-11 col-xs-offset-1"><i class="fa fa-wrench "></i> Compétences</h2>
+        <p class="col-xs-11 col-xs-offset-1">
+            Front Office : <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half-empty"></i><i class="fa fa-star-o"></i><br/>
+
+            Back Office : <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half-empty"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><br/>
+
+            Application Android : <i class="fa fa-star"></i><i class="fa fa-star-half-empty"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><br/>
+
+            Anglais : <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><br/>
+
+            Japonais : <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><br/>
+
+            Motivation : <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><br/>
+
+        </p>
+    </div>
+</section>
+
+
+<section id="section-4" class="col-xs-12 ">
+
+    <div class="row">
+        <h2 class="col-xs-11 col-xs-offset-1"><i class="fa fa-map-marker"></i> Directions</h2>
+        <p class="col-xs-11 col-xs-offset-1 col-md-7">
+            <span class="date-place"><i class="fa fa-train"></i> En train: </span><br/>
+            Station Parc des expositions <br/>
+            <span class="date-place"><i class="fa fa-bus"></i> En bus:</span><br/>
+            Lignes 349 et 350 <br/>
+            <span class="date-place"><i class="fa fa-train"></i> En avion: </span><br/>
+            Charles de Gaulle Etoile <br/>
+        </p>
+        <div id="map" class="col-xs-0 col-md-3"></div>
+    </div>
+</section>
+
+
+<section id="section-5" class="col-xs-12">
+
+    <div class="row">
+        <h2 class="col-xs-11 col-xs-offset-1"><i class="fa fa-slack"></i> Me contacter</h2>
+        <p class="col-xs-11 col-xs-offset-1">
+                <span class="date-place"><i class="fa fa-facebook-square"></i>
+                    <a href="https://www.facebook.com/mikal.field" title="Profile Facebook">Facebook</a>
+                </span><br/>
+
+            <span class="date-place"><i class="fa fa-whatsapp"></i>  Numéro : </span> 0698067299 <br/>
+
+            <span class="date-place"> <i class="fa fa-envelope"></i>    mail : </span> mikalfield@gmail.com
+        </p>
+    </div>
+</section>
+
+
+    <script>
+        function initMap() {
+            var uluru = {lat: 48.972404, lng: 2.516475};
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 15,
+                center: uluru
+            });
+            var marker = new google.maps.Marker({
+                position: uluru,
+                map: map
+            });
+        }
+    </script>
+    <script async defer
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyALh7YluDXBQqQcSGO2XO32z2Ob7j4Vya4&callback=initMap">
+    </script>
+</body>
+
+
+
+
+</html>
